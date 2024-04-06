ff6be1f04b5b52171e82766cf1e6807863ac32fe
Ammar Hany <ammarhanyezeldin@gmail.com>
2024-04-06 14:59:02 +0200

    Update partialLoader.js

--- scripts/partialLoader.js
+++ scripts/partialLoader.js
@@ -1,13 +1,22 @@
 // scripts/partialLoader.js
 
-// ! Loading Partial HTMLs in Index.html Start
-
 // Function to load partial HTML files into their respective sections
 function loadPartialHTML(url, sectionId) {
   fetch(url)
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById(sectionId).innerHTML = data;
+      const parser = new DOMParser();
+      const htmlDocument = parser.parseFromString(data, "text/html");
+      const fragment = document.createDocumentFragment();
+
+      // Append each element from the parsed HTML to the fragment
+      htmlDocument.body.childNodes.forEach((node) => {
+        fragment.appendChild(node.cloneNode(true));
+      });
+
+      // Replace the content of the section with the fragment
+      document.getElementById(sectionId).innerHTML = "";
+      document.getElementById(sectionId).appendChild(fragment);
     })
     .catch((error) => console.error("Error loading partial HTML:", error));
 }
