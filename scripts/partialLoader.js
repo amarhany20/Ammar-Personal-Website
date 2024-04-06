edc19a75a02e9ea51509af1a7096df94691b31c5
Ammar Hany <ammarhanyezeldin@gmail.com>
2024-04-06 15:09:31 +0200

    dynamic main.js loading

--- scripts/partialLoader.js
+++ scripts/partialLoader.js
@@ -4,27 +4,35 @@
 
 // Function to load partial HTML files into their respective sections
 function loadPartialHTML(url, sectionId) {
-  fetch(url)
-    .then((response) => response.text())
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to load partial HTML: " + response.status);
+      }
+      return response.text();
+    })
     .then((data) => {
       document.getElementById(sectionId).innerHTML = data;
     })
     .catch((error) => console.error("Error loading partial HTML:", error));
 }
 
-// Load partial HTML files into their respective sections
-loadPartialHTML("./partials/navbar.html", "navbar");
-loadPartialHTML("./partials/landing.html", "landing");
-loadPartialHTML("./partials/skills.html", "skills");
-loadPartialHTML("./partials/experience.html", "experience");
-loadPartialHTML("./partials/education.html", "education");
-loadPartialHTML("./partials/certificates.html", "certificates");
-loadPartialHTML("./partials/footer.html", "footer");
-
-// Load main.js after a delay
-setTimeout(() => {
-  const script = document.createElement("script");
-  script.src = "./scripts/main.js";
-  document.head.appendChild(script);
-}, 100); // 500 milliseconds delay
+// Load all partial HTML files
+Promise.all([
+  loadPartialHTML("../partials/navbar.html", "navbar"),
+  loadPartialHTML("../partials/landing.html", "landing"),
+  loadPartialHTML("../partials/skills.html", "skills"),
+  loadPartialHTML("../partials/experience.html", "experience"),
+  loadPartialHTML("../partials/education.html", "education"),
+  loadPartialHTML("../partials/certificates.html", "certificates"),
+  loadPartialHTML("../partials/footer.html", "footer"),
+])
+  .then(() => {
+    // All partial HTML files have been loaded successfully
+    // Load main.js
+    const script = document.createElement("script");
+    script.src = "./scripts/main.js";
+    document.head.appendChild(script);
+  })
+  .catch((error) => console.error("Error loading partial HTML:", error));
 // ! Loading Partial HTMLs in Index.html End
